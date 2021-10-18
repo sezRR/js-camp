43a85dc083a6dfd685ae5ed395b133473a67ed09
szRR <sezer.tetik2005@gmail.com>
2021-10-19 00:01:53 +0300

    add project files

---
+++ javaScriptStarterKit-master/javaScriptStarterKit-master/src/app.js
@@ -0,0 +1,36 @@
+console.log("Hello World!")
+
+// dollarYesterday = "Ankara"
+
+let dollarToday = 9.30
+let dollarYesterday = 9.20
+
+// JavaScript (JS) is not type safe
+dollarYesterday = "Ankara"
+
+{
+    let dollarYesterday = 9.10
+}
+
+console.log(dollarYesterday)
+
+const euroYesterday = 11.2
+// euroYesterday = 11 // error
+
+console.log(euroYesterday)
+
+// array
+// camelCase -> değişken tanımlama
+// PascalCase
+let konutKredileri = ["Konut Kredisi", "Emlak Konut Kredisi", "Kamut Konut Kredisi", "Özel Konut Kredisi"]
+
+console.log(konutKredileri)
+
+console.log("<ul>")
+
+for (let i = 0; i < konutKredileri.length; i++) {
+    console.log("<li>" + konutKredileri[i] + "</li>")
+}
+
+console.log("<ul>")
+
